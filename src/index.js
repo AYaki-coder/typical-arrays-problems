3e84fda3fc144b23ad3ac2c06227f97d8ae2ed54
ayaki-coder <sj.a.yakimovich@gmail.com>
2020-03-17 00:39:30 +0300

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  }
+  let n = array[0];
+  
+  array.forEach(element => {
+    n = n < element ? n : element;
+  });
+
+  return n;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  }
+  let n = array[0];
+  array.forEach(element => {
+
+    n = n > element ? n : element;
+  });
+
+  return n;
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+  if (array == undefined || array.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  array.map(element => {
+    sum += element;
+  });
+
+  return sum / array.length;
+
 }
+
+
+
+
+
